7e62dbdb3fc3c4c21c44b863e4d0174b27f5832e
dce4 <dogancanerbas@protonmail.com>
2020-11-25 13:14:49 +0300

    added slim display manager and greenclip

--- scripts/polybar/launch.sh
+++ scripts/polybar/launch.sh
@@ -1,12 +1,9 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar external >>/tmp/polybar1.log 2>&1 &
-polybar internal >>/tmp/polybar1.log 2>&1 &
-echo "Bars launched..."
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main >> /tmp/polybar1.log 2>&1 &
+  done
+else
+  polybar --reload main >> /tmp/polybar1.log 2>&1 &
+fi
